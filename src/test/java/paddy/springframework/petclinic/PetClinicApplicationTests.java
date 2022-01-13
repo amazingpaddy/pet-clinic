02cb7f8d47822d3c692118fd8ae8e2c66191f47c
Paddy <padmanabhan5789@gmail.com>
2022-01-13 18:43:30 -0500

    Initial Project Add

---
+++ src/test/java/paddy/springframework/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package paddy.springframework.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
