fa3a9b67b2d871374aab3d3ea65f16eb295c21b7
Paddy <padmanabhan5789@gmail.com>
2022-01-25 18:48:59 -0500

    Menu Links are broken. Closes #46.

--- pet-clinic-web/src/main/java/paddy/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/paddy/petclinic/controllers/OwnerController.java
@@ -19,4 +19,9 @@ public class OwnerController {
         model.addAttribute("owners", ownerService.findAll());
         return "owner/index";
     }
+
+    @RequestMapping("/find")
+    public String findOwners() {
+        return "notimplemented";
+    }
 }
