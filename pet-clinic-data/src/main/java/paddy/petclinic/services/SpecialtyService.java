dbcba248362a13c5491c98971cd1e4c11eefbb0e
Paddy <padmanabhan5789@gmail.com>
2022-01-25 17:56:12 -0500

    Create Specialty Map Service impl. Closes #32.

---
+++ pet-clinic-data/src/main/java/paddy/petclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package paddy.petclinic.services;
+
+import paddy.petclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/paddy/petclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,35 @@
+package paddy.petclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import paddy.petclinic.model.Specialty;
+import paddy.petclinic.services.SpecialtyService;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findByID(id);
+    }
+}
