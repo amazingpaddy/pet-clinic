d1bf4f0f0d1b64aa054df038db6dc66e4e0cb310
Paddy <padmanabhan5789@gmail.com>
2022-01-28 12:47:21 -0500

    Create Visit Entity. Closes #39.

--- pet-clinic-data/src/main/java/paddy/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/paddy/petclinic/model/Pet.java
@@ -2,53 +2,66 @@ package paddy.petclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
-    @Column(name = "name")
-    private String name;
+  @Column(name = "name")
+  private String name;
 
-    @ManyToOne
-    @JoinColumn(name = "type_id")
-    private PetType petType;
+  @ManyToOne
+  @JoinColumn(name = "type_id")
+  private PetType petType;
 
-    @ManyToOne
-    @JoinColumn(name = "owner_id")
-    private Owner owner;
+  @ManyToOne
+  @JoinColumn(name = "owner_id")
+  private Owner owner;
 
-    @Column(name = "birth_date")
-    private LocalDate birthDate;
+  @Column(name = "birth_date")
+  private LocalDate birthDate;
 
-    public String getName() {
-        return name;
-    }
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+  private Set<Visit> visits = new HashSet<>();
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public PetType getPetType() {
-        return petType;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public void setPetType(PetType petType) {
-        this.petType = petType;
-    }
+  public PetType getPetType() {
+    return petType;
+  }
 
-    public Owner getOwner() {
-        return owner;
-    }
+  public void setPetType(PetType petType) {
+    this.petType = petType;
+  }
 
-    public void setOwner(Owner owner) {
-        this.owner = owner;
-    }
+  public Owner getOwner() {
+    return owner;
+  }
 
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
+  public void setOwner(Owner owner) {
+    this.owner = owner;
+  }
 
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
+  public LocalDate getBirthDate() {
+    return birthDate;
+  }
+
+  public void setBirthDate(LocalDate birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  public Set<Visit> getVisits() {
+    return visits;
+  }
+
+  public void setVisits(Set<Visit> visits) {
+    this.visits = visits;
+  }
 }
--- pet-clinic-data/src/main/java/paddy/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/paddy/petclinic/model/Visit.java
@@ -1,33 +1,42 @@
 package paddy.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
-    private LocalDate date;
-    private String description;
-    private Pet pet;
+  @Column(name = "date")
+  private LocalDate date;
 
-    public LocalDate getDate() {
-        return date;
-    }
+  @Column(name = "description")
+  private String description;
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
+  @ManyToOne
+  @JoinColumn(name = "pet_id")
+  private Pet pet;
 
-    public String getDescription() {
-        return description;
-    }
+  public LocalDate getDate() {
+    return date;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public void setDate(LocalDate date) {
+    this.date = date;
+  }
 
-    public Pet getPet() {
-        return pet;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public void setPet(Pet pet) {
-        this.pet = pet;
-    }
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Pet getPet() {
+    return pet;
+  }
+
+  public void setPet(Pet pet) {
+    this.pet = pet;
+  }
 }
