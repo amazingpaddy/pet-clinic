8382c86646adfcf16ce3200f3d13b7e99bd66fbb
Paddy <padmanabhan5789@gmail.com>
2022-01-28 18:11:48 -0500

    Create Visit Map Service . Closes #48.

---
+++ pet-clinic-data/src/main/java/paddy/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,44 @@
+package paddy.petclinic.services.map;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.stereotype.Service;
+import paddy.petclinic.model.Visit;
+import paddy.petclinic.services.VisitService;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+  @Override
+  public Set<Visit> findAll() {
+    return super.findAll();
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    super.deleteById(id);
+  }
+
+  @Override
+  public void delete(Visit object) {
+    super.delete(object);
+  }
+
+  @Override
+  public Visit save(Visit visit) {
+    if (ObjectUtils.anyNull(
+        visit.getPet(),
+        visit.getPet().getOwner(),
+        visit.getPet().getId(),
+        visit.getPet().getOwner().getId())) {
+      throw new RuntimeException("Not a valid visit");
+    }
+    return super.save(visit);
+  }
+
+  @Override
+  public Visit findById(Long aLong) {
+    return super.findByID(aLong);
+  }
+}
