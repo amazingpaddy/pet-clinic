0e787a3232d969efae30c6d23bfdb9f6e41f1e54
Paddy <padmanabhan5789@gmail.com>
2022-01-28 22:43:05 -0500

    Add Project lombok and refactor. Closes #52

--- pet-clinic-data/src/main/java/paddy/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/paddy/petclinic/model/Person.java
@@ -1,28 +1,26 @@
 package paddy.petclinic.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity {
-    @Column(name = "first_name")
-    private String firstName;
-    @Column(name = "last_name")
-    private String lastName;
+  @Column(name = "first_name")
+  private String firstName;
 
-    public String getFirstName() {
-        return firstName;
-    }
+  @Column(name = "last_name")
+  private String lastName;
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+  public Person(Long id, String firstName, String lastName) {
+    super(id);
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 }
--- pet-clinic-data/src/main/java/paddy/petclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/paddy/petclinic/model/Specialty.java
@@ -1,21 +1,22 @@
 package paddy.petclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "specialities")
 public class Specialty extends BaseEntity {
 
-    @Column(name = "description")
-    private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  @Column(name = "description")
+  private String description;
 }
