6ad413aba379c6aa5fd41735c29574cbbd0a3a21
Paddy <padmanabhan5789@gmail.com>
2022-01-29 09:49:10 -0500

    Applied reverted fix

--- pet-clinic-data/src/main/java/paddy/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/paddy/petclinic/model/Vet.java
@@ -1,23 +1,36 @@
 package paddy.petclinic.model;
 
-import javax.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
-            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
-    private Set<Specialty> specialties = new HashSet<>();
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(
+      name = "vet_specialities",
+      joinColumns = @JoinColumn(name = "vet_id"),
+      inverseJoinColumns = @JoinColumn(name = "speciality_id"))
+  private Set<Specialty> specialties = new HashSet<>();
 
-    public Set<Specialty> getSpecialties() {
-        return specialties;
-    }
-
-    public void setSpecialties(Set<Specialty> specialties) {
-        this.specialties = specialties;
-    }
+  @Builder
+  public Vet(Long id, String firstName, String lastName, Set<Specialty> specialties) {
+    super(id, firstName, lastName);
+    this.specialties = specialties;
+  }
 }
