f7dd45db504bf0478dff964e1f753b8cd39ff59c
Paddy <padmanabhan5789@gmail.com>
2022-02-18 16:08:22 -0500

    Implemented Vets json EndPoint. Closes #65

--- pet-clinic-web/src/main/java/paddy/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/paddy/petclinic/controllers/VetController.java
@@ -2,8 +2,13 @@ package paddy.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import paddy.petclinic.model.Vet;
 import paddy.petclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -18,4 +23,9 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
+
+  @GetMapping("/api/vets")
+  public @ResponseBody Set<Vet> getVetsJson() {
+    return vetService.findAll();
+  }
 }
